ac26eb35c059d79475c18a3249a1ac1bfe358ecc
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2023-02-08 17:33:33 -0300

    Adicionado as validacoes para categoria

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,15 +1,43 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string? Name { get; set; }
+        public int Id { get; private set; }
+        public string? Name { get; private set; }
+    
+        public Category(string name)
+        {
+            ValidationDomain(name);
+        }
+
+        public Category(string name, int id)
+        {
+            ValidationDomain(name);
+            DomainExceptionValidation.When(
+                id <= 0, "Invalid Id value");
+            Id = id;
+        }
         public ICollection<Product> Products { get; set; }
+
+        public void Update(string name)
+        {
+            ValidationDomain(name);
+        }
+        private void ValidationDomain(string name)
+        {
+            DomainExceptionValidation.When(
+                string.IsNullOrEmpty(name),
+                "Invalid name. Name is required"
+                );
+
+            DomainExceptionValidation.When(
+                name.Length < 3,
+                "Invalid name, too short, minimum 3 characters"
+                );
+            Name = name;
+        }
     }
 }

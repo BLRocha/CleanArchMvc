eed11aec43d9619ba9f1b751802acccb2749084c
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2023-02-20 16:21:32 -0300

    Category Testes

--- CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
+++ CleanArchMvc.Domain.Tests/CategoryUnitTest1.cs
@@ -1,4 +1,5 @@
 using CleanArchMvc.Domain.Entities;
+using FluentAssertions;
 
 namespace CleanArchMvc.Domain.Tests
 {
@@ -8,7 +9,16 @@ namespace CleanArchMvc.Domain.Tests
         public void CreateCategory_WithValidParameters_ResultObjValidState()
         {
             Action action = () => new Category(1, "Category Name");
+            action.Should()
+                .NotThrow<CleanArchMvc.Domain.Validation.DomainExceptionValidation>();
+        }
 
+        [Fact]
+        public void CreateCategory_NegativeIdValue_DomainExceptionValidation()
+        {
+            Action action = () => new Category(-1, "Category Name");
+            action.Should().Throw<CleanArchMvc.Domain.Validation.DomainExceptionValidation>()
+                .WithMessage("Invalid Id value");
         }
     }
 }
